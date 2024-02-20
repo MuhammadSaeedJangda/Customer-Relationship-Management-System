ae635d5e65618e8a8bfaea81fe7e9708e5c3901e
Muhammad Saeed Jangda <muhammadsaeedjangda@gmail.com>
2024-02-20 17:16:36 +0500

    update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 import 'registration.dart';
 import 'login.dart';
+import 'crm.dart';
+import 'user.dart';
 
 void main() {
   print("WELCOME TO CUSTOMER RELATIONSHIP MANAGEMENT SYSTEM");
   Registration registration = Registration();
   Login login = Login(registration.userList);
+  CRM myCRM = CRM();
+  User? loggedInUser;
+
   print("---------------------------------------------------");
 
   //For User Registration
@@ -32,4 +37,32 @@ void main() {
 
   login.loginUser(loginEmail, loginPassword);
   print("------------- THANKS FOR LOGIN -------------");
+
+  loggedInUser = login.loginUser(loginEmail, loginPassword);
+
+  // If the user is logged in, allow adding a new customer
+  if (loggedInUser != null && loggedInUser.name.isNotEmpty) {
+    print("--------- Enter customer details: ---------");
+    print("Name: ");
+    String customerName = stdin.readLineSync()!;
+
+    print("Email: ");
+    String customerEmail = stdin.readLineSync()!;
+
+    print("Phone Number: ");
+    String customerPhoneNumber = stdin.readLineSync()!;
+
+    print("Country: ");
+    String customerCountry = stdin.readLineSync()!;
+
+    // Add the new customer
+    myCRM.addCustomer(loggedInUser, customerName, customerEmail,
+        customerPhoneNumber, customerCountry);
+
+    // Display customers
+    print("--------- Customer Display Section ---------");
+    myCRM.displayCustomers();
+  } else {
+    print("Login failed. Please check your email and password.");
+  }
 }
