399e534eba4bea94a6938762888029160c70471c
Muhammad Saeed Jangda <muhammadsaeedjangda@gmail.com>
2024-02-20 17:16:20 +0500

    Update login.dart

--- login.dart
+++ login.dart
@@ -5,14 +5,14 @@ class Login {
 
   Login(this.userList);
 
-  bool loginUser(String email, String password) {
+  User? loginUser(String email, String password) {
     for (var user in userList) {
       if (user.email == email && user.password == password) {
         print("Login successful! Welcome, ${user.name}!");
-        return true;
+        return user;
       }
     }
     print("Invalid email or password. Login failed.");
-    return false;
+    return null; // Return null if login fails
   }
 }
